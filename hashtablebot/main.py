7a37767f738826fb451d4368170ce4ab113a91ad
Douglas C <douglasc.dev@gmail.com>
2022-09-08 15:16:32 -0300

    Use rst-style docstring links for main function

--- hashtablebot/main.py
+++ hashtablebot/main.py
@@ -33,14 +33,14 @@ def main(token, channels, log_level):
     The entry point for the bot's code.
 
     Installing the bot with pip or setup.py and running "hashtablebot" should lead to this function executing,
-    as specified in the project's [setup file](https://github.com/douglascdev/hashtablebot/blob/343154016d1e32002ab417ab8f52456e967fe803/setup.py#L21).
+    as specified in the project's `setup file <https://github.com/douglascdev/hashtablebot/blob/343154016d1e32002ab417ab8f52456e967fe803/setup.py#L21>`_
 
-    The parameters/environment variables are gathered here by [Click](https://click.palletsprojects.com/en/8.1.x/)
+    The parameters/environment variables are gathered here by `Click <https://click.palletsprojects.com/en/8.1.x/>`_
     and passed to the HashTableBot class.
 
-    :param token: the OAuth Access Token provided by twitch(see https://dev.twitch.tv/docs/authentication/getting-tokens-oauth).
+    :param token: the `OAuth Access Token<https://dev.twitch.tv/docs/authentication/getting-tokens-oauth>`_ given by twitch.
     :param channels: A comma-separated list of twitch channels the bot should join
-    :param log_level: set the logging level(see https://docs.python.org/3/library/logging.html#levels).
+    :param log_level: set the `logging level <https://docs.python.org/3/library/logging.html#levels>`_
     :return:
     """
     logging.basicConfig(level=log_level)
