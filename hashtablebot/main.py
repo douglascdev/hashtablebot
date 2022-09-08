d94acd3b41866adbe076d9b10b9b98f192db42b2
Douglas C <douglasc.dev@gmail.com>
2022-09-08 13:02:06 -0300

    Use click to gather environment variables or accept command line arguments instead

--- hashtablebot/main.py
+++ hashtablebot/main.py
@@ -1,19 +1,51 @@
 import logging
-import os
+import click
 
 from hashtablebot.hash_table_bot import HashTableBot
 
 
-def main():
-    # Get token and initial channels and log level from environment variables
-    twitch_token = os.environ.get("TWITCH_TOKEN")
-    comma_separated_initial_channels = os.environ.get(
-        "COMMA_SEPARATED_INITIAL_CHANNELS", "hash_table"
-    )
-    log_level = os.environ.get("LOG_LEVEL", "INFO")
+@click.command(help="HashTableBot")
+@click.option(
+    "--token",
+    envvar="TOKEN",
+    help="OAuth Access Token provided by twitch(see https://dev.twitch.tv/docs/authentication/getting-tokens-oauth). "
+    "Can also be passed with the TOKEN environment variable",
+    required=True,
+)
+@click.option(
+    "--channels",
+    envvar="CHANNELS",
+    help="A comma-separated list of twitch channels the bot should join. "
+    "Can also be passed with the CHANNELS environment variable.",
+    required=True,
+)
+@click.option(
+    "--log_level",
+    envvar="LOG_LEVEL",
+    default="INFO",
+    help="Set the logging level, default is INFO. Can also be passed with the LOG_LEVEL environment variable.",
+    type=(
+        click.Choice(("CRITICAL", "FATAL", "ERROR", "WARN", "WARNING", "INFO", "DEBUG"))
+    ),
+)
+def main(token, channels, log_level):
+    """
+    The entry point for the bot's code.
+
+    Installing the bot with pip or setup.py and running "hashtablebot" should lead to this function executing,
+    as specified in the project's [setup file](https://github.com/douglascdev/hashtablebot/blob/343154016d1e32002ab417ab8f52456e967fe803/setup.py#L21).
+
+    The parameters/environment variables are gathered here by [Click](https://click.palletsprojects.com/en/8.1.x/)
+    and passed to the HashTableBot class.
+
+    :param token: the OAuth Access Token provided by twitch(see https://dev.twitch.tv/docs/authentication/getting-tokens-oauth).
+    :param channels: A comma-separated list of twitch channels the bot should join
+    :param log_level: set the logging level(see https://docs.python.org/3/library/logging.html#levels).
+    :return:
+    """
     logging.basicConfig(level=log_level)
 
-    bot = HashTableBot(twitch_token, comma_separated_initial_channels.split(","))
+    bot = HashTableBot(token, channels.split(","))
     bot.run()
 
 
