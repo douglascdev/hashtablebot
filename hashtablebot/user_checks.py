b03c1a9e72ee38f1fc7a6bd225d48e4a3214bc6a
Douglas C <douglasc.dev@gmail.com>
2022-09-09 12:14:52 -0300

    Remove async keyword from user checks

--- hashtablebot/user_checks.py
+++ hashtablebot/user_checks.py
@@ -1,13 +1,13 @@
 from twitchio import Chatter, PartialChatter
 
 
-async def is_bot_admin(user: Chatter):
+def is_bot_admin(user: Chatter):
     admins = {"hash_table"}
     return user.name in admins
 
 
-async def is_bot_admin_or_mod(user: Chatter):
-    return user and user.is_mod or await is_bot_admin(user)
+def is_bot_admin_or_mod(user: Chatter):
+    return user and user.is_mod or is_bot_admin(user)
 
 
 def is_valid_chatter(user: Chatter | PartialChatter | None):
