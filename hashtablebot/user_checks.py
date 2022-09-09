6728e9a2e8b153552b8f860e40bebfc5700b80e0
Douglas C <douglasc.dev@gmail.com>
2022-09-09 18:57:14 -0300

    Renamed is_valid_chatter to is_chatter

--- hashtablebot/user_checks.py
+++ hashtablebot/user_checks.py
@@ -10,5 +10,5 @@ def is_bot_admin_or_mod(user: Chatter):
     return user and user.is_mod or is_bot_admin(user)
 
 
-def is_valid_chatter(user: Chatter | PartialChatter | None):
+def is_chatter(user: Chatter | PartialChatter | None):
     return user and type(user) == Chatter
