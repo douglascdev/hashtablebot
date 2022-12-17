614806a8267782aff16aaa790a2099403a9e9b27
Douglas C <douglasc.dev@gmail.com>
2022-12-17 16:00:43 -0300

    Remove explicit database creation from session causing errors in sphinx

--- hashtablebot/persistence/session.py
+++ hashtablebot/persistence/session.py
@@ -1,9 +1,7 @@
 from os import getenv
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-from hashtablebot.entity.bot_user import BotUser
+from sqlalchemy.orm import sessionmaker
 
 _db_user = getenv("POSTGRES_USER")
 _db_password = getenv("POSTGRES_PASSWORD")
@@ -14,5 +12,3 @@ engine = create_engine(f"postgresql+psycopg2://{_db_user}:{_db_password}@{_db_ho
 
 # TODO: search: is it ok to use expire_on_commit like this?
 Session = sessionmaker(engine, future=True, expire_on_commit=False)
-
-declarative_base().metadata.create_all(engine, tables=[BotUser.__table__])
