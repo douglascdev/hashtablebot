8458a34ff5309920029c0a890828369f34f38376
Douglas C <douglasc.dev@gmail.com>
2022-12-17 00:36:09 -0300

    Use env vars to load database configuration passed by docker

--- hashtablebot/persistence/session.py
+++ hashtablebot/persistence/session.py
@@ -1,9 +1,16 @@
+from os import getenv
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 from hashtablebot.entity.bot_user import BotUser
 
-engine = create_engine("sqlite:///hashtablebot.db", future=True)
+_db_user = getenv("POSTGRES_USER")
+_db_password = getenv("POSTGRES_PASSWORD")
+_db_name = getenv("POSTGRES_DB")
+_db_host = getenv("POSTGRES_HOST")
+
+engine = create_engine(f"postgresql+psycopg2://{_db_user}:{_db_password}@{_db_host}/{_db_name}", future=True)
 
 # TODO: search: is it ok to use expire_on_commit like this?
 Session = sessionmaker(engine, future=True, expire_on_commit=False)
